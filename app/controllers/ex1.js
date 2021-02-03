9fdf57ce2b5fb667b7e27c10016a703e4246a6b2
LucasGuillemette <51739149+LucasGuillemette@users.noreply.github.com>
2021-02-04 00:57:12 +0100

    Vue finit

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -4,21 +4,28 @@ import { tracked } from '@glimmer/tracking';
 
 export default class Ex1Controller extends Controller {
   @tracked content = 'Default';
-  @tracked info = 'a';
+  @tracked info = '';
+  @tracked hide = 'none';
   MAX = 100;
   nbRestant = 100;
+
+
+
 
   get size() {
     return this.MAX - this.content.length;
   }
   get style() {
-    if (this.MAX - this.content.length > 10) {
+    if(this.MAX - this.content.length === 100){
+      this.hide = 'none';
+    }
+    if (this.MAX - this.content.length >= 50) {
       return 'alert-info';
     }
-    else if(this.MAX - this.content.length < 0){
+    else if(this.MAX - this.content.length < 20){
       return 'alert-danger';
     }
-    else{
+    else if(this.MAX - this.content.length < 50){
       return 'alert-warning';
     }
   }
@@ -26,6 +33,7 @@ export default class Ex1Controller extends Controller {
   @action
   clear() {
     this.content = '';
+    this.hide = 'none';
   }
 
   @action
@@ -35,11 +43,16 @@ export default class Ex1Controller extends Controller {
 
   @action
   save() {
+    this.hide = 'visible';
     this.info = 'Note enregistrée';
   }
 
   @action
   update() {
-    this.info = this.info + 'Note modifiée';
+    this.hide = 'visible';
+    this.info = 'Note modifiée';
+
   }
+
+
 }
