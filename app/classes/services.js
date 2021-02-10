c6af0405ef4fee89a0fba93fceb42c7bd8adbaef
LucasGuillemette <51739149+LucasGuillemette@users.noreply.github.com>
2021-02-10 15:50:05 +0100

    route ex2 complete

---
+++ app/classes/services.js
@@ -0,0 +1,20 @@
+import { computed } from '@ember/object';
+
+export default class Services{
+
+  services = [];
+
+  constructor(serveices) {
+    this.services = serveices;
+  }
+
+  @computed('services.@each.active')
+  get countActive(){
+    /*let r=0;
+    this.services.forEach(service=>{
+      if(service.active)
+        r++;
+    });
+    return r;*/
+    return this.services.filterBy('active', true).length;
+  }
